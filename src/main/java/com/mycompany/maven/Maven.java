7c71f161d2745bbe2cfe68b6f610a435c709fc8c
muhammad <muhammad@Ubuntu>
2023-02-05 16:53:28 +0300

    this if first commit

---
+++ src/main/java/com/mycompany/maven/Maven.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maven;
+
+/**
+ *
+ * @author muhammad
+ */
+public class Maven {
+
+    public static void main(String[] args) {
+        int i = 50;
+        System.out.println(i);
+    }
+}
